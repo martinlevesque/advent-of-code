f3450d6f326bfc7c9d4eb44a50e02cecad27014c
Martin Levesque <levesque.martin@gmail.com>
2023-12-05 14:49:15 -0500

    wip

--- 2023/day2/src/main.zig
+++ 2023/day2/src/main.zig
@@ -8,6 +8,10 @@ pub const GameSet = struct {
     pub fn init() GameSet {
         return GameSet{ .nb_blue = 0, .nb_red = 0, .nb_green = 0 };
     }
+
+    pub fn multiplied(self: GameSet) i32 {
+        return self.nb_blue * self.nb_red * self.nb_green;
+    }
 };
 
 pub const Game = struct {
@@ -125,6 +129,38 @@ pub const Game = struct {
 
         return sets;
     }
+
+    // part 1:
+    pub fn is_possible(self: *Game, nb_red: i32, nb_green: i32, nb_blue: i32) bool {
+        for (self.sets) |set| {
+            if (set.nb_red > nb_red or set.nb_green > nb_green or set.nb_blue > nb_blue) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    // part 2:
+    pub fn minimum_set_cubes(self: *Game) GameSet {
+        var set = GameSet.init();
+
+        for (self.sets) |s| {
+            if (s.nb_blue > set.nb_blue) {
+                set.nb_blue = s.nb_blue;
+            }
+
+            if (s.nb_red > set.nb_red) {
+                set.nb_red = s.nb_red;
+            }
+
+            if (s.nb_green > set.nb_green) {
+                set.nb_green = s.nb_green;
+            }
+        }
+
+        return set;
+    }
 };
 
 pub fn main() !void {
@@ -139,6 +175,8 @@ pub fn main() !void {
     var in_stream = buf_reader.reader();
 
     var buf: [100000]u8 = undefined;
+    var sum_possible_games: i32 = 0;
+    var sum_min_sets: i32 = 0;
 
     while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
         std.log.info("line = {s}\n", .{line});
@@ -146,8 +184,23 @@ pub fn main() !void {
         var game = try Game.init(allocator, line);
         defer game.deinit();
 
+        if (game.is_possible(12, 13, 14)) {
+            sum_possible_games += game.id;
+        }
+
         std.log.info("game -> {any}\n", .{game});
+
+        var min_set = game.minimum_set_cubes();
+        var set_multiplied = min_set.multiplied();
+
+        sum_min_sets += set_multiplied;
+
+        std.log.info("min set -> {any}\n", .{min_set});
+        std.log.info("set multiplied -> {d}\n", .{set_multiplied});
     }
+
+    std.log.info("sum possible games {d}\n", .{sum_possible_games});
+    std.log.info("sum_min_sets {d}\n", .{sum_min_sets});
 }
 
 test "game_id_part_to_int happy path" {
@@ -161,4 +214,24 @@ test "game_id_part_to_int invalid" {
     var result = Game.game_id_part_to_int(game_id_part);
 
     try std.testing.expectError(error.GameIdNotFound, result);
+}
+
+test "parse_sets_part" {
+    var allocator = std.testing.allocator;
+    var sets = try Game.parse_sets_part(
+        allocator,
+        "10 blue, 12 red; 8 red; 7 green, 5 red, 7 blue"
+    );
+    defer allocator.free(sets);
+
+    try std.testing.expectEqual(sets.len, 3);
+    try std.testing.expectEqual(sets[0].nb_blue, 10);
+    try std.testing.expectEqual(sets[0].nb_red, 12);
+    try std.testing.expectEqual(sets[0].nb_green, 0);
+    try std.testing.expectEqual(sets[1].nb_blue, 0);
+    try std.testing.expectEqual(sets[1].nb_red, 8);
+    try std.testing.expectEqual(sets[1].nb_green, 0);
+    try std.testing.expectEqual(sets[2].nb_blue, 7);
+    try std.testing.expectEqual(sets[2].nb_red, 5);
+    try std.testing.expectEqual(sets[2].nb_green, 7);
 }
