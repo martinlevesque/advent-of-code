c21041796549a56fd7065acf0ae6549128fb2ad8
Martin Levesque <levesque.martin@gmail.com>
2024-12-02 20:49:26 -0500

    day 2, p1

---
+++ 2024/2/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func reportSafe(report []string) bool {
+
+	direction := ""
+
+	for index, item := range report {
+		if index == len(report)-1 {
+			continue
+		}
+
+		currentItemI, _ := strconv.Atoi(item)
+		nextItemI, _ := strconv.Atoi(report[index+1])
+
+		if nextItemI == currentItemI {
+			return false
+		}
+
+		currentDirection := ""
+
+		if nextItemI < currentItemI {
+			currentDirection = "decreasing"
+		} else {
+			currentDirection = "increasing"
+		}
+
+		if direction == "" {
+			if nextItemI < currentItemI {
+				direction = "decreasing"
+			} else {
+				direction = "increasing"
+			}
+		} else if currentDirection != direction {
+			return false
+		}
+
+		distance := math.Abs(float64(currentItemI) - float64(nextItemI))
+
+		if distance < 1 || distance > 3 {
+			return false
+		}
+	}
+
+	return true
+}
+
+func main() {
+	log.Println("yo.")
+
+	file, err := os.Open("input.txt")
+
+	if err != nil {
+		fmt.Printf("Error opening file: %v\n", err)
+		return
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	if err := scanner.Err(); err != nil {
+		fmt.Printf("Error reading file: %v\n", err)
+	}
+
+	sumPart1 := 0
+
+	for scanner.Scan() {
+		line := scanner.Text() // Get the current line as a string
+		// Process the line
+		fmt.Println(line) // Example: print each line
+		parts := strings.Fields(line)
+
+		if reportSafe(parts) {
+			sumPart1 += 1
+		}
+	}
+
+	log.Println("sum part 1 ", sumPart1)
+}
