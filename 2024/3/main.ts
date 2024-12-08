93da05aef48246d00edb8d5f9a5f993f2f84b8a3
Martin Levesque <levesque.martin@gmail.com>
2024-12-08 10:39:48 -0500

    3

--- 2024/3/main.ts
+++ 2024/3/main.ts
@@ -2,13 +2,38 @@ import * as fs from "node:fs";
 
 const fileContent = fs.readFileSync("input.txt", "utf-8");
 
-const regex = /mul\((\d{1,3}),(\d{1,3})\)/g;
+function compute(content: string, withActivation: boolean): number {
+	const regexWithActivation =
+		/(mul\((\d{1,3}),(\d{1,3})\))|(do\(\))|(don't\(\))/g;
+	const regexWithoutActivation = /mul\((\d{1,3}),(\d{1,3})\)/g;
 
-let match;
-let result: number = 0;
+	let regex: RegExp = regexWithActivation;
 
-while ((match = regex.exec(fileContent)) !== null) {
-	result += parseInt(match[1]) * parseInt(match[2]);
+	if (!withActivation) {
+		regex = regexWithoutActivation;
+	}
+
+	let match;
+	let result: number = 0;
+	let activated: boolean = true;
+
+	while ((match = regex.exec(content)) !== null) {
+		if (!withActivation) {
+			result += parseInt(match[1]) * parseInt(match[2]);
+		} else {
+			if (match[0] === "do()") {
+				activated = true;
+			} else if (match[0] === "don't()") {
+				activated = false;
+			} else if (activated) {
+				result += parseInt(match[2]) *
+					parseInt(match[3]);
+			}
+		}
+	}
+
+	return result;
 }
 
-console.log(`result part 1: ${result}`);
+console.log(`result part 1: ${compute(fileContent, false)}`);
+console.log(`result part 2: ${compute(fileContent, true)}`);
