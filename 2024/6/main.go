fdf80e3c7ea2c9e1fa5b4665e9d82acaa867c85f
Martin Levesque <levesque.martin@gmail.com>
2024-12-29 14:48:27 -0500

    6 2

--- 2024/6/main.go
+++ 2024/6/main.go
@@ -28,87 +28,161 @@ func PrintMapping(mapping *[][]rune) {
 	}
 }
 
-func findNextMovePosition(guardPosition Position, mapping *[][]rune) *Position {
+func isObstruction(box rune) bool {
+	return box == '#' || box == 'O'
+}
+
+func guardCanGoInBox(box rune) bool {
+	return box == '.' || box == 'X'
+}
+
+func cloneMapping(mapping *[][]rune) [][]rune {
+	original := *mapping
+	clone := make([][]rune, len(original)) // Create a new top-level slice with the same length.
+
+	for i := range original {
+		// Copy each inner slice.
+		clone[i] = make([]rune, len(original[i]))
+		copy(clone[i], original[i])
+	}
+
+	return clone
+}
+
+func isLoop(guardPosition Position, mapping *[][]rune) bool {
+	currentPosition := guardPosition
+	history := []Position{}
+
+	for {
+		next, loopDetected := findNextMovePosition(currentPosition, mapping, &history)
+
+		history = append(history, currentPosition)
+
+		if loopDetected {
+			return true
+		}
+
+		if next == nil {
+			break
+		} else {
+			currentPosition = *next
+		}
+	}
+
+	return false
+}
+
+func findLoops(guardPosition Position, mapping *[][]rune) int {
+	result := 0
+
+	for y, line := range *mapping {
+		for x, item := range line {
+			if item == 'X' && !(y == guardPosition.Y && x == guardPosition.X) {
+				mappingWithObstruction := cloneMapping(mapping)
+				mappingWithObstruction[y][x] = 'O'
+
+				if isLoop(guardPosition, &mappingWithObstruction) {
+					result += 1
+				}
+			}
+		}
+	}
+
+	return result
+}
+
+func hasBeenTo(currentPosition Position, history *[]Position) bool {
+	for _, p := range *history {
+		if p.X == currentPosition.X && p.Y == currentPosition.Y && p.Direction == currentPosition.Direction {
+			return true
+		}
+	}
+
+	return false
+}
+
+func findNextMovePosition(guardPosition Position, mapping *[][]rune, history *[]Position) (*Position, bool) {
 	newPosition := guardPosition
 	var box rune
 	lineLen := len((*mapping)[0])
 	nbLines := len(*mapping)
 
+	if hasBeenTo(guardPosition, history) {
+		return nil, true
+	}
+
 	if guardPosition.Direction == "up" {
 		if guardPosition.Y == 0 {
-			return nil // found the solution
+			return nil, false
 		}
 
 		box = (*mapping)[guardPosition.Y-1][guardPosition.X]
 
-		if box == '.' || box == 'X' {
+		if guardCanGoInBox(box) {
 			newPosition.Y = guardPosition.Y - 1
-			return &newPosition
-		} else if box == '#' {
+			return &newPosition, false
+		} else if isObstruction(box) {
 			newPosition.Direction = "right"
-			return findNextMovePosition(newPosition, mapping)
+			return findNextMovePosition(newPosition, mapping, history)
 		}
 
 	} else if guardPosition.Direction == "right" {
 		if guardPosition.X == lineLen-1 {
-			return nil
+			return nil, false
 		}
 
 		box = (*mapping)[guardPosition.Y][guardPosition.X+1]
 
-		if box == '.' || box == 'X' {
+		if guardCanGoInBox(box) {
 			newPosition.X = guardPosition.X + 1
-			return &newPosition
-		} else if box == '#' {
+			return &newPosition, false
+		} else if isObstruction(box) {
 			newPosition.Direction = "down"
-			return findNextMovePosition(newPosition, mapping)
+			return findNextMovePosition(newPosition, mapping, history)
 		}
 	} else if guardPosition.Direction == "down" {
 		if guardPosition.Y == nbLines-1 {
-			return nil
+			return nil, false
 		}
 
 		box = (*mapping)[guardPosition.Y+1][guardPosition.X]
 
-		if box == '.' || box == 'X' {
+		if guardCanGoInBox(box) {
 			newPosition.Y = guardPosition.Y + 1
-			return &newPosition
-		} else if box == '#' {
+			return &newPosition, false
+		} else if isObstruction(box) {
 			newPosition.Direction = "left"
-			return findNextMovePosition(newPosition, mapping)
+			return findNextMovePosition(newPosition, mapping, history)
 		}
 	} else if guardPosition.Direction == "left" {
 		if guardPosition.X == 0 {
-			return nil
+			return nil, false
 		}
 
 		box = (*mapping)[guardPosition.Y][guardPosition.X-1]
 
 		if box == '.' || box == 'X' {
 			newPosition.X = guardPosition.X - 1
-			return &newPosition
-		} else if box == '#' {
+			return &newPosition, false
+		} else if isObstruction(box) {
 			newPosition.Direction = "up"
-			return findNextMovePosition(newPosition, mapping)
+			return findNextMovePosition(newPosition, mapping, history)
 		}
 	} else {
-		log.Println("else here")
 		box = '-'
 	}
 
-	return nil
+	return nil, false
 }
 
 func countMovesUntilOutside(guardPosition Position, mapping *[][]rune) int {
-	for {
-		PrintPosition(guardPosition)
-		PrintMapping(mapping)
-		(*mapping)[guardPosition.Y][guardPosition.X] = 'X'
-		next := findNextMovePosition(guardPosition, mapping)
+	history := []Position{}
 
-		if next == nil {
-			PrintPosition(guardPosition)
-			PrintMapping(mapping)
+	for {
+		(*mapping)[guardPosition.Y][guardPosition.X] = 'X'
+		next, loopDetected := findNextMovePosition(guardPosition, mapping, &history)
+
+		if next == nil || loopDetected {
 			break
 		} else {
 			guardPosition = *next
@@ -149,7 +223,6 @@ func main() {
 		Y:         -1,
 		Direction: "up",
 	}
-	result := 0
 	mapping := [][]rune{}
 	curY := 0
 
@@ -169,8 +242,9 @@ func main() {
 		curY += 1
 	}
 
-	log.Println("mapping = ", mapping)
-	result = countMovesUntilOutside(guard, &mapping)
+	resultPart1 := countMovesUntilOutside(guard, &mapping)
+	log.Println("result part 1 = ", resultPart1)
+	resultPart2 := findLoops(guard, &mapping)
 
-	log.Println("result = ", result)
+	log.Println("result part 2 = ", resultPart2)
 }
