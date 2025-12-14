e6d51d7190adf2fac026a69f8abc4c588cf984b0
Martin Levesque <levesque.martin@gmail.com>
2025-12-14 11:11:31 -0500

    3

--- 2025/3.py
+++ 2025/3.py
@@ -1,4 +1,7 @@
-def max_joltage(input_s: str) -> int:
+from itertools import combinations
+
+
+def max_joltage_part_1(input_s: str) -> int:
     result = -1
 
     for i, cur_ch in enumerate(input_s):
@@ -11,6 +14,24 @@ def max_joltage(input_s: str) -> int:
     return result
 
 
+def max_joltage_part_2(input_s: str) -> int:
+    result = ""
+    nb_digits_to_have = 12
+    nb_to_remove = len(input_s) - nb_digits_to_have
+
+    for cur_ch in input_s:
+        while result and nb_to_remove > 0 and result[-1] < cur_ch:
+            result = result[:-1]
+            nb_to_remove -= 1
+
+        result += cur_ch
+
+    if nb_to_remove > 0:
+        result = result[:-nb_to_remove]
+
+    return int(result)
+
+
 def main():
     try:
         with open("3.txt", "r") as f:
@@ -19,9 +40,10 @@ def main():
             result = 0
 
             for line in file_content.splitlines():
+                print("=============================")
                 print(f"line = {line} {len(line)}")
 
-                cur_max = max_joltage(line)
+                cur_max = max_joltage_part_2(line)
                 print(f"max = {cur_max}")
                 result += cur_max
 
